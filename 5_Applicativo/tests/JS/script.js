2979621a55983d55d5b27dc5978d55a59ba4e482
ChiodoniGioele <gioele.chiodoni@samtrevano.ch>
2023-09-15 11:21:11 +0200

    Commit at 11:21:10.97  15.09.2023 - Test draw puntini with num

--- 5_Applicativo/tests/JS/script.js
+++ 5_Applicativo/tests/JS/script.js
@@ -1,33 +1,73 @@
+// classe che rappresenta e disegna il puntino salvato nel array points
+
 class Point{
-    constructor(event, canvas){
+
+    constructor(event, canvas, numPoint){
 
     // Coordinate globali click
-        var eventX = event.clientX;
-        var eventY = event.clientY;
+        let eventX = event.clientX;
+        let eventY = event.clientY;
 
     // coordinate canvas 
-        var xCanvasLeft = canvas.getBoundingClientRect().left;
-        var yCanvasTop = canvas.getBoundingClientRect().top;
+        let xCanvasLeft = canvas.getBoundingClientRect().left;
+        let yCanvasTop = canvas.getBoundingClientRect().top;
 
-    // stampa le coordinate del click
-        console.log("Cordinata X: " + this.getXClick(eventX, xCanvasLeft));
-        console.log("Cordinata Y: " + this.getYClick(eventY, yCanvasTop));
+    // le coordinate del click
+        let x = this.getXClick(eventX, xCanvasLeft);
+        let y = this.getYClick(eventY, yCanvasTop);
 
+    // disegno il puntino
+        this.drawPoint(canvas, x, y, numPoint + 1);
     }
 
+    // ritorna la posizione del puntino X
     getXClick(xE, xC){
         return  xE- xC;
     }
     
+    // ritorna la posizione del puntino Y
     getYClick(yE, yC){
         return yE - yC;
     }
+
+    drawPoint(canvas, x, y, num, color, size) {
+        // rendo disegnabile il canvas
+            var canvasDrawed = canvas.getContext('2d');
+        // se dati non inseriti
+            if (color == null) {
+                color = '#000';
+            }
+                if (size == null) {
+                size = 5;
+            }
+
+        // disegno il punt
+            canvasDrawed.beginPath();
+            canvasDrawed.fillStyle = color;
+            canvasDrawed.arc(x, y, size, 0 * Math.PI, 2 * Math.PI);
+            canvasDrawed.fill();
+        
+        // se ce il testo
+            if (num) {
+                // posizione testo
+                    var textX = x + 2;
+                    var textY = Math.round(y - size - 3);
+                // stampo il testo
+                    canvasDrawed.font = 'Italic 14px Arial';
+                    canvasDrawed.fillStyle = color;
+                    canvasDrawed.textAlign = 'center';
+                    canvasDrawed.fillText(num, textX, textY);
+            }
+    }
 }
 
-let canvas = document.getElementById("canvas");
+// canvas
+    let canvas = document.getElementById("canvas");
 
+// array con i puntini
+    var points = new Array();
 
 // on mouse click over canvas
     canvas.addEventListener("mousedown",function(e){
-        var p = new Point(e, canvas);
+        points.push(new Point(e, canvas, points.length));
     });
